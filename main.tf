58a2d02f5f85ab8c114c58150b4ced7e471920ee
harshap0202 <02harshpatil@gmail.com>
2025-03-20 17:28:36 +0530

    commit two

--- main.tf
+++ main.tf
@@ -5,14 +5,14 @@ provider "aws" {
 
 # s3
 module "s3-rdm-ui" {
-  source      = "./s3-rdm-ui"
+  source      = "./modules/s3-rdm-ui"
   bucket_name = var.bucket_name
   prefix      = var.prefix
 }
 
 # cloudfront / OAC / Bucket Policy
 module "cloudfront-rdm-ui" {
-  source             = "./cloudfront-rdm-ui"
+  source             = "./modules/cloudfront-rdm-ui"
   prefix             = var.prefix
   bucket_domain_name = module.s3-rdm-ui.bucket_domain_name
   bucket_arn         = module.s3-rdm-ui.bucket_arn
